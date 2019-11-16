241ac98c0b59f401532249801670b37b036277fb
hasinthaK <hasiroxsola@gmail.com>
2019-11-16 15:08:20 +0530

    Roles checkboxes binding not done

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NewUser } from 'src/app/models/new-user';
+import { UserServiceService } from 'src/app/services/user-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newuser = new NewUser();
+  USER: true;
+  ADMIN: false;
+
+  constructor(private userS: UserServiceService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  // addRole(role: string) {
+  //   this.newuser.roles.push(role + `,`);
+  // }
+
+  register() {
+    console.log(this.newuser);
+    // this.userS.register(this.newuser)
+    // .subscribe(
+    //   res => {
+    //     console.log(res);
+    //     this.router.navigate(['/profile']);
+    //   },
+    //   err => {
+    //     console.log(err);
+    //   }
+    // );
   }
 
 }
